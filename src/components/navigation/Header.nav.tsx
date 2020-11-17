61c3e8cf249ba138845b51fab1d995fe6bb4dcf3
Vatamanyuk Mykola <mykola796@gmail.com>
2020-11-17 16:58:42 +0200

    update header

--- src/components/navigation/Header.nav.tsx
+++ src/components/navigation/Header.nav.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react'
 import { useHistory } from 'react-router'
 import { AppBar, Toolbar, IconButton, Avatar } from '@material-ui/core'
-import ShoppingBasket from '@material-ui/icons/ShoppingBasket'
+
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 
 import { useAuth } from '../../context/auth.context'
+
+import { LocationStatus } from '../location/Location.status'
 
 import { useStyles } from './styles'
 
@@ -20,11 +23,12 @@ export const Header: FC = () => {
             className={styles.appBar}
         >
             <Toolbar className={styles.toolBar}>
+                <LocationStatus />
                 <IconButton 
                     aria-label="menu" 
                     color='secondary'
                 >
-                    <ShoppingBasket />
+                    <ShoppingBasketIcon />
                 </IconButton>
                 <IconButton  
                     color="inherit" 
