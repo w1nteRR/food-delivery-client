7fe266838e3222d4c60a867a545aeaf0c6f08a74
Vatamanyuk Mykola <mykola796@gmail.com>
2020-11-17 16:59:17 +0200

    auth deps fix

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -26,6 +26,7 @@ export const Auth: FC = () => {
                 }
             })()
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isAuthenticated])
 
     if(isLoading) return (
