5675935bb89ebacd7330df3c594e5e560a41ac60
Vatamanyuk Mykola <mykola796@gmail.com>
2020-11-26 17:52:22 +0200

    update home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,20 +2,49 @@ import React, { FC } from 'react'
 import { Box, Container } from '@material-ui/core'
 
 import { RestaurantCard } from '../../components/cards/Restaurant.card'
+import { FoodNav } from '../../components/navigation/Food.nav'
+
+import restaurants from '../../utils/data/restaurants.json'
+
+console.log(restaurants)
 
 export const Home: FC = () => {
     return (
-        <Container maxWidth='lg'>
-            <Box
+        <Container 
+            maxWidth='lg' 
+        >
+            <FoodNav />
+           <Box
                 display='flex'
+                flexGrow='0'
+                flexShrink='1'
                 flexWrap='wrap'
                 justifyContent='center'
-            >
-                <RestaurantCard name='test' />
-                <RestaurantCard name='test' />
-                <RestaurantCard name='test' />
-                <RestaurantCard name='test' />
-            </Box>
+           >
+                {
+                    restaurants.map((rest, index) => 
+                        <RestaurantCard 
+                            key={index} 
+                            name={rest.name} 
+                            image={rest.image} 
+                            tags={rest.tags || ['']} 
+                            deliveryPrice={rest.deliveryPrice}
+                            deliveryTime={rest.deliveryTime}
+                            _id={rest._id}
+                        />
+                    )
+                }
+
+                <div style={filter}></div>
+                <div style={filter}></div>
+                <div style={filter}></div>
+
+           </Box>
         </Container>
     )
+}
+
+const filter = {
+    width: 375,
+    margin: 10
 }
