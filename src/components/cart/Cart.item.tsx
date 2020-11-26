46f2eac5ab476d1d5dce39482595a2ea40c1ad4d
Vatamanyuk Mykola <mykola796@gmail.com>
2020-11-26 17:50:35 +0200

    add cart item

---
+++ src/components/cart/Cart.item.tsx
@@ -0,0 +1,63 @@
+import React, { FC } from 'react'
+import { Box, IconButton, Paper, Typography, FormControl, Select, MenuItem } from '@material-ui/core'
+
+import DeleteIcon from '@material-ui/icons/Delete'
+
+interface CartItemProps {
+    name: string
+    count: number
+    price: string
+    onDeleteClick?: () => void
+    onSelectChange: (e: any) => void
+}
+
+export const CartItem: FC<CartItemProps> = ({
+    name,
+    count,
+    price,
+    onDeleteClick,
+    onSelectChange
+}) => 
+   <Paper elevation={2} style={{
+       margin: '20px 0'
+   }}>
+        <Box
+            p={3}
+            display='flex'
+            justifyContent='space-between'
+            flexDirection='column'
+        >
+            <Box display='flex' justifyContent='space-between' alignItems='center'>
+            <Typography
+                variant='overline'
+            >
+                {name}
+            </Typography>
+            <Typography>
+                {price}
+            </Typography>
+            </Box>
+            <Box>
+                <FormControl variant="outlined">
+                <Select
+                    value={count}
+                    onChange={e => onSelectChange(e.target.value)}
+                    style={{
+                        height: '30px'
+                    }}
+                >
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                </Select>
+                </FormControl>
+                <IconButton  
+                    aria-label="delete" 
+                    color="secondary"
+                    onClick={onDeleteClick}
+                >
+                    <DeleteIcon fontSize='small' />
+                </IconButton>
+            </Box>
+       </Box>
+   </Paper>
