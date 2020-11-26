458c3ad9be9efa9b676abc2892941540f9851b56
Vatamanyuk Mykola <mykola796@gmail.com>
2020-11-26 17:49:52 +0200

    update menu list

--- src/components/restaurant/menu/Menu.list.tsx
+++ src/components/restaurant/menu/Menu.list.tsx
@@ -3,47 +3,60 @@ import { Box, Typography } from '@material-ui/core'
 
 import { IMenu } from '../../../interfaces/menu/IMenu'
 import { MenuCard } from './Menu.card'
+import { useCart } from '../../../context/cart.context'
 
 
 export const MenuList: FC<{ menu: Array<IMenu> }> = ({
     menu
-}) => 
-   <>
-   {
-        menu.map((section, index) => 
-            <Box key={index} m={1}>
-                <Typography
-                    variant='h5'
-                    component='h2'
-                >
-                    {section.sectionName}
-                </Typography>
-                <Box
-                    display='flex'
-                    flexGrow='0'
-                    flexShrink='1'
-                    flexWrap='wrap'
-                    justifyContent='center'
-                >
-                    {
-                        section.items.map(item => 
-                            <MenuCard
-                                key={item._id}
-                                name={item.name}
-                                image={item.image}
-                                price={item.price}
-                                description={item.description}
-                            />
-                        )
-                    }
-                    <div style={filter} />
-                    <div style={filter} />
-                    <div style={filter} />
+}) => {
+
+    const { addToCart } = useCart()
+
+    return (
+        <>
+        {
+            menu.map((section, index) => 
+                <Box key={index} m={1}>
+                    <Typography
+                        variant='h5'
+                        component='h2'
+                    >
+                        {section.sectionName}
+                    </Typography>
+                    <Box
+                        display='flex'
+                        flexGrow='0'
+                        flexShrink='1'
+                        flexWrap='wrap'
+                        justifyContent='center'
+                    >
+                        {
+                            section.items.map(item => 
+                                <MenuCard
+                                    key={item._id}
+                                    name={item.name}
+                                    image={item.image}
+                                    price={item.price}
+                                    description={item.description}
+                                    onPriceClick={() => addToCart({
+                                        name: item.name,
+                                        price: item.price,
+                                        count: 1,
+                                        _id: item._id
+                                    })}
+                                />
+                            )
+                        }
+                        <div style={filter} />
+                        <div style={filter} />
+                        <div style={filter} />
+                    </Box>
                 </Box>
-            </Box>
-            )
+                )
         }
-   </>
+        </>
+    )
+}
 
 const filter = {
     width: 300,
